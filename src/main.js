03e1bc8e130dc34e7f665d259de5000b037bdb27
inc904 <jay1845@aliyun.com>
2024-10-23 17:49:24 +0800

    feat✨: 安装tailwindcss

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
 import router from './router'
+
+// 一定要在main.ts中导入tailwind.css，防止vite每次hmr都会请求src/style/index.scss整体css文件导致热更新慢的问题
+import './style/tailwind.css'
 import './style.css'
+
 import App from './App.vue'
 
 createApp(App).use(router).mount('#app')
--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div>DashBoard</div>
+  <h1 className="text-3xl font-bold underline">Hello world!</h1>
 </template>
 
 <script setup>

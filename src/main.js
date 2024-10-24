0fe49ef92dcbf9ed579bb6f383a78de9485067ad
inc904 <jay1845@aliyun.com>
2024-10-24 17:41:01 +0800

    feat✨: tailwindcss修复

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 
+import './style/index.scss'
 // 一定要在main.ts中导入tailwind.css，防止vite每次hmr都会请求src/style/index.scss整体css文件导致热更新慢的问题
 import './style/tailwind.css'
-import './style/index.scss'
 import 'element-plus/dist/index.css'
 
 import router from './router'
